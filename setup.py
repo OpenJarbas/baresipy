0798152be719aa541635609648a95ab322f884ad
jarbasal <jarbasai@mailfence.com>
2020-04-25 15:56:04 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bareispy',
+    version='0.1',
+    packages=['baresipy'],
+    url='https://github.com/JarbasAl/baresipy',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='python baresip wrapper'
+)
