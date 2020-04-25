05e9087a36c83f969be516b1634748dfbc40cc8f
jarbasal <jarbasai@mailfence.com>
2020-04-25 16:01:29 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup
 setup(
     name='bareispy',
     version='0.1',
-    packages=['baresipy'],
+    packages=['baresipy', 'baresipy.utils'],
     url='https://github.com/JarbasAl/baresipy',
+    install_requires=["ResponsiveVoice",
+                      "opentone>=0.1.2",
+                      "pydub",
+                      "pexpect"],
     license='MIT',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
